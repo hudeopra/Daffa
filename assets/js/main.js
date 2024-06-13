b7f78f3126633cb017f1b8b92323f6708be84ea9
hudeopra <maharjanhudeo@gmail.com>
2024-06-13 20:17:43 +0545

    added login as popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,10 @@
 jQuery(document).ready(function ($) {
-  var nav_offset_top = $("header").height() - window.innerHeight * 0.95; // Adjusted to 10vh scroll
 
-  function navbarFixed() {
-    if ($(".ph-header").length) {
-      $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if (scroll >= nav_offset_top) {
-          $(".ph-header").addClass("header_fixed");
-          $(".ph-login").removeClass("ph-active");
-        } else {
-          $(".ph-header").removeClass("header_fixed");
-          $(".ph-login").removeClass("ph-active");
-        }
-      });
-    }
-  }
-
-  navbarFixed();
-
-  // // Toggle meny form visibility
-  // $(document).on("click", ".ph-header__open--trigger", function () {
-  //   $(".ph-header").toggleClass("header_fixed");
-  //   $(".ph-login").removeClass("ph-active");
-  // });
+// triggring login popup
+  $(document).on("click", "#loginBtn, .pop-up,.pop-up__close", function () {
+    $(".ph-login-form").toggleClass("active");
+  });
+  
 
   // // Toggle login form visibility
   // $(document).on("click", ".ph-user-trigger", function () {
